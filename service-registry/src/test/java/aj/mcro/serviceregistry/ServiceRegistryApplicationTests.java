3a662cb1c4ad9a57af20e027ba2d60cee6ab6d7a
Karan <aj91063@gmail.com>
2024-01-09 13:08:57 +0530

    Adding Eureka as a Service-Registry

---
+++ service-registry/src/test/java/aj/mcro/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package aj.mcro.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
