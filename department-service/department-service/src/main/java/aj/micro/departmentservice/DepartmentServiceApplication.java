0132b2d2ea84bf005870fd17d2491bc0cc669228
Karan <aj91063@gmail.com>
2024-01-09 16:14:50 +0530

    Adding Api-Gateway as a client with Service-Registry Routes the api of services

--- department-service/department-service/src/main/java/aj/micro/departmentservice/DepartmentServiceApplication.java
+++ department-service/department-service/src/main/java/aj/micro/departmentservice/DepartmentServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DepartmentServiceApplication {
-
+	//java -jar -Dserver.port=8083 department-service-0.0.1-SNAPSHOT.jar
 	public static void main(String[] args) {
 		SpringApplication.run(DepartmentServiceApplication.class, args);
 	}
