64246456a18b3f2f026186819cdfe7df01f2408c
Karan <aj91063@gmail.com>
2024-01-13 11:36:16 +0530

    Adding new Organigation Service

---
+++ organigation-service/src/main/java/aj/micro/organigationService/OrganigationServiceApplication.java
@@ -0,0 +1,13 @@
+package aj.micro.organigationService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganigationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrganigationServiceApplication.class, args);
+	}
+
+}
---
+++ organigation-service/src/test/java/aj/micro/organigationService/OrganigationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package aj.micro.organigationService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrganigationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
