da6ac85dfba107db48cf537002bb220b310eefd4
Karan <aj91063@gmail.com>
2024-01-17 16:39:56 +0530

    Event-driven-Kafka Stock-Service and Email-service is done

---
+++ Event-Driven Kafka Services/stock-service/src/main/java/com/micro/stockService/OrderConsumerKafka.java
@@ -0,0 +1,23 @@
+package com.micro.stockService;
+
+import com.micro.baseDomains.dto.Order;
+import com.micro.baseDomains.dto.OrderEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderConsumerKafka {
+        private static final Logger LOGGER= LoggerFactory.getLogger(OrderConsumerKafka.class);
+
+        private KafkaTemplate<String, OrderEvent> kafkaTemplate;
+
+        @KafkaListener(topics = "${spring.kafka.topic.name}", groupId = "${spring.kafka.consumer.group-id}")
+        public void consume(OrderEvent orderEvent){
+                LOGGER.info(String.format("Order Event Consume => %s",orderEvent));
+        }
+
+
+}
