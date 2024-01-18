32f191ed8575af4eb713b5409ab782e502fe1caa
Karan <aj91063@gmail.com>
2024-01-18 18:24:42 +0530

    Adding RabbitMQ spring project demo

---
+++ spring-rabbitMQ/src/test/java/aj/micro/springRabbitMQ/SpringRabbitMqApplicationTests.java
@@ -0,0 +1,13 @@
+package aj.micro.springRabbitMQ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRabbitMqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
