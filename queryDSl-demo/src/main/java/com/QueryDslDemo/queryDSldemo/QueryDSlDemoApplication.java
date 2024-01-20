947133be5c409c2189e1e33b0a780f77bf989ff8
Karan <aj91063@gmail.com>
2024-01-20 14:29:30 +0530

    Demo for query-dsl

---
+++ queryDSl-demo/src/main/java/com/QueryDslDemo/queryDSldemo/QueryDSlDemoApplication.java
@@ -0,0 +1,13 @@
+package com.QueryDslDemo.queryDSldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueryDSlDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueryDSlDemoApplication.class, args);
+	}
+
+}
---
+++ queryDSl-demo/src/test/java/com/QueryDslDemo/queryDSldemo/QueryDSlDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.QueryDslDemo.queryDSldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueryDSlDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
